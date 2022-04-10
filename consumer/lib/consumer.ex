358cc418ddc8221fd8cecdd5ea0d42d5daba6768
Luca Fruzza <lfruzza@hotmail.co.uk>
2022-04-10 15:58:14 +0100

    :construction: in-progress template

--- consumer/lib/consumer.ex
+++ consumer/lib/consumer.ex
@@ -12,7 +12,34 @@ defmodule Consumer do
       :world
 
   """
-  def hello do
-    :world
+def start() do
+    group_config = [
+      offset_commit_policy: :commit_to_kafka_v2,
+      offset_commit_interval_seconds: 5,
+      rejoin_delay_seconds: 2,
+      reconnect_cool_down_seconds: 10
+    ]
+
+    config = %{
+      client: :kafka_client,
+      group_id: "from_zero",
+      topics: ["test_topic"],
+      cb_module: __MODULE__,
+      group_config: group_config,
+      consumer_config: [begin_offset: :earliest]
+    }
+
+    :brod.start_link_group_subscriber_v2(config)
   end
+
+  def init(_arg, _arg2) do
+    {:ok, []}
+  end
+
+  def handle_message(message, state) do
+    {_kafka_message_set, content, partition, _unkow, _set} = message
+    IO.inspect("I recieved a message:")
+    IO.inspect(content)
+    {:ok, :commit, []}
+    end
 end
--- producer/lib/producer.ex
+++ producer/lib/producer.ex
@@ -12,7 +12,40 @@ defmodule Producer do
       :world
 
   """
-  def hello do
-    :world
+def publish(topic, partition_key, message) do
+    {:ok, count} = :brod.get_partitions_count(:kafka_client, topic)
+
+    :brod.produce_sync(
+      :kafka_client,
+      topic,
+      :erlang.phash2(partition_key, count),
+      partition_key,
+      message
+    )
+  end
+
+  def start(_, _) do
+    create("test_topic")
+    {:ok, spawn(&looper/0)}
+  end
+
+  defp looper(), do: looper()
+
+defp create(name) do
+    topic_config = [
+      %{
+        config_entries: [],
+        num_partitions: 6,
+        replica_assignment: [],
+        replication_factor: 1,
+        topic: name
+      }
+    ]
+
+    :brod.create_topics(
+      ["localhost": 29092],
+      topic_config,
+      %{timeout: 1_000}
+    )
   end
 end
